25f0f15dbc1373966135bfe97f19837c1aa87722
Shane <6071159+smashedr@users.noreply.github.com>
2026-05-03 11:07:56 -0700

    Updates

--- src/main.py
+++ src/main.py
@@ -11,30 +11,29 @@ input_version = os.environ.get("INPUT_VERSION")
 input_addon_id = os.environ.get("INPUT_ADDON_ID")
 
 
-if not input_manifest and not (input_version or input_addon_id):
-    raise ValueError("You must provide a manifest or both version and addon_id")
-
-
 print(f"📄 Using update file: {input_update}")
 
 if os.path.isfile(input_manifest):
-    print(f"Reading manifest: {input_manifest}")
+    print(f"📚 Reading manifest file: {input_manifest}")
     with open(input_manifest) as f:
         manifest = json.load(f)
+elif not input_version or not input_addon_id:
+    raise ValueError("You must provide a manifest or both version and addon_id")
+
 
 version = input_version or manifest["version"]
-print(f"  Addon Version:   {version}")
+print(f"    Addon Version:  {version}")
 addon_id = input_addon_id or manifest["browser_specific_settings"]["gecko"]["id"]
-print(f"  Addon ID:        {addon_id}")
+print(f"         Addon ID:  {addon_id}")
 url = input_url.format(version=version)
-print(f"  Update URL:      {url}")
+print(f"       Update URL:  {url}")
 
 if os.path.isfile(input_update):
     print(f"📚 Reading Update File: {input_update}")
     with open(input_update) as f:
         result = json.load(f)
 else:
-    print(f"⚠️ Update File Not Found! Creating: {input_update}")
+    print(f"::warning::⚠️ Creating New Update File: {input_update}")
     result = {"addons": {addon_id: {"updates": []}}}
 
 addition = {
